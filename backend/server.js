c9d63368e7b9bfadd2537dc306f0cdf9c9cd0d65
Rahul Gupta <rahulguptacsit@gmail.com>
2025-10-07 22:55:28 +0530

    chnges

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,10 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
+app.get("/", (req, res) => {
+  res.send("TaskBoard Backend is Running...");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
